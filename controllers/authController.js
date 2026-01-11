56283397f78c5df11ee96811c4cf5e8f5727227f
arthsonani <arthsonani@gmail.com>
2026-01-11 12:10:31 +0530

    user fetach final fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,10 +17,10 @@ export const authenticateUser = (req, res, next) => {
 
 export const signup = async (req, res) => {
   console.log('Signup attempt:', req.body);
-  const { name, email, password } = req.body;
+  const { username, email, password } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ name, email, password: hashedPassword });
+    const user = new User({ name: username, email, password: hashedPassword });
     await user.save();
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
